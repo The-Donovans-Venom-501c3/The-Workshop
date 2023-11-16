09abf4068ead9e0695f864c9bd17d4686ec61259
ShimiRokach <shimi3601@gmail.com>
2023-11-16 21:39:50 +0200

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Iframe from 'react-iframe'
+import '../../App.css';
+
+export default function Home() {
+    return (
+        <div align="center" className='container'>
+            <Iframe
+                //url='https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvZnJwYW50aGVyYV90aWdyaXNfYWx0YWljYV90aWdlcl8wLWltYWdlLWt6eGx2YzYyLmpwZw.jpg'
+                width="100%"
+                height="100%"
+                scrolling="no"
+                id=""
+                className=""
+                display="block"
+                position="relative" />
+
+            <canvas className='topcanvas' ></canvas>
+        </div>
+    )
+}
