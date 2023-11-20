826644d5722e55fd91a34a8e444b5690222dc0e1
Hung Le <45841230+hungle2396@users.noreply.github.com>
2023-11-20 01:44:46 -0800

    Made specific color palette appear for specific tool chosen, and hide the other color palettes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import ToolSelection from "./pages/ToolSelection/ToolSelection";
 import { useState } from "react";
 
 function App() {
-
     const [toggle, setToggle] = useState(false);
+
+    const handleToggle = () => {
+        setToggle((preToggle) => !preToggle);
+    };
 
     return (
         <div className="app">
             <Home />
-            <button className="orange--button" onClick={() => setToggle(true)}>Use Book</button>
+            <button className="orange--button" onClick={handleToggle}>
+                {toggle ? "Use Book" : "Interact!"}
+            </button>
             {toggle && <ToolSelection />}
         </div>
     );
