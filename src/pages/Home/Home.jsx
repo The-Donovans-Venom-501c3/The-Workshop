b3691ae08a433ce02b474d2501a85718a3da16c2
ShimiRokach <shimi3601@gmail.com>
2023-11-21 23:31:12 +0200

    canvas and bugs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Iframe from "react-iframe";
+import { ReactSketchCanvas } from 'react-sketch-canvas';
 import "../../App.css";
 
 export default function Home() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get("id");
+
+    const styles = {
+        border: '0.0625rem solid #9c9c9c',
+        borderRadius: '0.25rem',
+    };
 
     return (
         <div align="center" className="container">
@@ -15,7 +21,14 @@ export default function Home() {
                 className="iframe"
             />
 
-            <canvas className="topcanvas"></canvas>
+            {/* <canvas className="topcanvas"></canvas> */}
+            <ReactSketchCanvas
+                style={styles}
+                width="600"
+                height="400"
+                strokeWidth={4}
+                strokeColor="red"
+            />
         </div>
     );
 }
