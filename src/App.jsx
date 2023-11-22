6badc73ccd7ee3b868efb8bd6b5189a1bf707ee3
Hung Le <45841230+hungle2396@users.noreply.github.com>
2023-11-22 02:59:39 -0800

    Apply color to each tool, implemented the eraser feature, and added new svg images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import ToolSelection from "./pages/ToolSelection/ToolSelection";
-import { useState } from "react";
+import EraseImage from "./assets/eraser.png";
+import { useState, useRef } from "react";
 
 function App() {
-    const [colorHex, setColorHex] = useState("#43da86");
+    const [colorHex, setColorHex] = useState("#000000");
+    const [fontSize, setFontSize] = useState(0);
     const [toggle, setToggle] = useState(false);
+
+    const sketchRef = useRef(null);
 
     const handleToggle = () => {
         setToggle((preToggle) => !preToggle);
@@ -13,11 +17,18 @@ function App() {
 
     return (
         <div className="app">
-            <Home color={colorHex} />
+            <Home color={colorHex} fontSize={fontSize} sketchRef={sketchRef} />
             <button className="orange--button" onClick={handleToggle}>
                 {toggle ? "Use Book" : "Interact!"}
             </button>
-            {toggle && <ToolSelection setColor={setColorHex} />}
+            {toggle && (
+                <ToolSelection
+                    setColor={setColorHex}
+                    setFont={setFontSize}
+                    colorHex={colorHex}
+                    sketchRef={sketchRef}
+                />
+            )}
         </div>
     );
 }
