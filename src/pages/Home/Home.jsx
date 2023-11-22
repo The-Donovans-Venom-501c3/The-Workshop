8321e4fbdb4dc51002b006068f304aa85b0dee00
ShimiRokach <shimi3601@gmail.com>
2023-11-22 16:13:57 +0200

    add text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Iframe from "react-iframe";
 import { ReactSketchCanvas } from "react-sketch-canvas";
 import "../../App.css";
@@ -7,12 +7,40 @@ export default function Home({ color, fontSize, sketchRef }) {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get("id");
+    const [val, setVal] = useState([]);
+    const [idx, setIdx] = useState(null);
 
-    console.log("fontSize:", fontSize);
+    //console.log("fontSize:", fontSize);
+    console.log(idx);
 
+    const handleAdd = () => {
+        const abc = [...val, []]
+        setVal(abc)
+    }
+    const handleChange = (onChangeValue, i) => {
+        const inputdata = [...val]
+        inputdata[i] = onChangeValue.target.value;
+        setVal(inputdata)
+    }
+    const handleDelete = () => {
+        const deletVal = [...val]
+        deletVal.splice(idx, 1)
+        setVal(deletVal)
+    }
     return (
         <>
             <div align="center" className="container">
+
+                <div className="overlay">
+                    {val.map((data, i) => {
+                        return (
+                            <div className="try" key={i} onClick={() => setIdx(i)}>
+                                <input value={data} onChange={e => handleChange(e, i)} />
+                            </div>
+                        )
+                    })}
+                </div>
+
                 <Iframe
                     url={`https://heyzine.com/flip-book/${id}.html`}
                     allowfullscreen="allowfullscreen"
@@ -28,6 +56,9 @@ export default function Home({ color, fontSize, sketchRef }) {
                     canvasColor="transparent"
                     className="canvas"
                 />
+
+                <button onClick={() => handleAdd()}>Add</button>
+                <button onClick={() => handleDelete()}>x</button>
             </div>
         </>
     );
